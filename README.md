365ffb6be5e29c9301f2f222228ee11165798730
ceca69ec <86930570+ceca69ec@users.noreply.github.com>
2021-07-16 19:31:00 +0000

    Inclusion of examples and help on documentation.

--- README.md
+++ README.md
@@ -12,6 +12,26 @@ mnemonic39
 
     **Use at your won risk.**
 
+## Examples
+
+```console
+$ mnemonic39 06e9375a0b76cb5de997f93d6c707508 -p "バンドメイド"
+alone endless story black hole puzzle play you dice glory bubble awkward
+016b1146bb155c7c4ad0de6172bd4b7b6e65a487411c015c19512b8182e98e42fba7cc9da1c7631445fbfaea70057243c52d337c2d7be51786f5023086ae1bf7
+xprv9s21ZrQH143K2pFLbGtCAETJEGS9egv9K1AhjHaACu22pEp5CUoSgFCwHYTfUE3aPKPgwccAJqneZoHX1J6iRvkkbxTuYdNhGSUHqfoWzDy
+yprvABrGsX5C9jant7STRdfpNKYoQEabbJueE7gvWgU3auPusLdJT8y1JJs5JkRFU8hVnxWVh6CimW9CT5u5izWjEASMUJAL8YCBYAXwEGcYVem
+zprvAWgYBBk7JR8GjQdaFzTSaQeJaCj3Xvu99ED9J5MvxumnvSSXho8ZvNXDKxNqU3MRCbdJSZoHEAVkLNWeSgvk2Q7xLdrkiT1fotbacrsgQox
+```
+
+```console
+$ mnemonic39 alone endless story black hole puzzle play you dice glory bubble awkward -p "バンドメイド"
+06e9375a0b76cb5de997f93d6c707508
+016b1146bb155c7c4ad0de6172bd4b7b6e65a487411c015c19512b8182e98e42fba7cc9da1c7631445fbfaea70057243c52d337c2d7be51786f5023086ae1bf7
+xprv9s21ZrQH143K2pFLbGtCAETJEGS9egv9K1AhjHaACu22pEp5CUoSgFCwHYTfUE3aPKPgwccAJqneZoHX1J6iRvkkbxTuYdNhGSUHqfoWzDy
+yprvABrGsX5C9jant7STRdfpNKYoQEabbJueE7gvWgU3auPusLdJT8y1JJs5JkRFU8hVnxWVh6CimW9CT5u5izWjEASMUJAL8YCBYAXwEGcYVem
+zprvAWgYBBk7JR8GjQdaFzTSaQeJaCj3Xvu99ED9J5MvxumnvSSXho8ZvNXDKxNqU3MRCbdJSZoHEAVkLNWeSgvk2Q7xLdrkiT1fotbacrsgQox
+```
+
 ## Features
 
 * **Generate a list of valid last words**
@@ -62,6 +82,40 @@ mnemonic39
 
     Insert mnemonic and target language flag to result in words in the same 'position' on the
  new word-list.
+
+## Help
+
+```bash
+mnemonic39 1.0.2
+Enter optional language and hexadecimal entropy with optional passphrase or
+enter a list of mnemonic words with optional passphrase to see information
+about it. It can show a list of valid last words to fulfil a list of mnemonic
+words missing the last one. Generate mnemonic using a list of valid words.
+
+USAGE:
+    mnemonic39 [FLAGS] [OPTIONS] <DATA>...
+
+FLAGS:
+    -c, --chinese        Mnemonic with chinese simplified words
+    -e, --english        Mnemonic with english words
+    -f, --french         Mnemonic with french words
+    -g, --generate       Generate valid mnemonics permuting words
+    -h, --help           Prints help information
+    -i, --italian        Mnemonic with italian words
+    -j, --japanese       Mnemonic with japanese words
+    -k, --korean         Mnemonic with korean words
+    -o, --portuguese     Mnemonic with portuguese words
+    -s, --spanish        Mnemonic with spanish words
+    -t, --traditional    Mnemonic with chinese traditional words
+    -V, --version        Prints version information
+    -z, --czech          Mnemonic with czech words
+
+OPTIONS:
+    -p <passphrase>        optional passphrase to be used with the mnemonic
+
+ARGS:
+    <DATA>...    hexadecimal entropy, seed or a list of mnemonic words
+```
 
 ## What this tool *don't* do
 * **Generate entropy**
--- src/lib.rs
+++ src/lib.rs
@@ -9,12 +9,33 @@
 //!
 //!     **Use at your won risk.**
 //!
+//! ## Examples
+//!
+//! ```console
+//! $ mnemonic39 06e9375a0b76cb5de997f93d6c707508 -p "バンドメイド"
+//! alone endless story black hole puzzle play you dice glory bubble awkward
+//! 016b1146bb155c7c4ad0de6172bd4b7b6e65a487411c015c19512b8182e98e42fba7cc9da1c7631445fbfaea70057243c52d337c2d7be51786f5023086ae1bf7
+//! xprv9s21ZrQH143K2pFLbGtCAETJEGS9egv9K1AhjHaACu22pEp5CUoSgFCwHYTfUE3aPKPgwccAJqneZoHX1J6iRvkkbxTuYdNhGSUHqfoWzDy
+//! yprvABrGsX5C9jant7STRdfpNKYoQEabbJueE7gvWgU3auPusLdJT8y1JJs5JkRFU8hVnxWVh6CimW9CT5u5izWjEASMUJAL8YCBYAXwEGcYVem
+//! zprvAWgYBBk7JR8GjQdaFzTSaQeJaCj3Xvu99ED9J5MvxumnvSSXho8ZvNXDKxNqU3MRCbdJSZoHEAVkLNWeSgvk2Q7xLdrkiT1fotbacrsgQox
+//! ```
+//!
+//! ```console
+//! $ mnemonic39 alone endless story black hole puzzle play you dice glory bubble awkward -p "バンドメイド"
+//! 06e9375a0b76cb5de997f93d6c707508
+//! 016b1146bb155c7c4ad0de6172bd4b7b6e65a487411c015c19512b8182e98e42fba7cc9da1c7631445fbfaea70057243c52d337c2d7be51786f5023086ae1bf7
+//! xprv9s21ZrQH143K2pFLbGtCAETJEGS9egv9K1AhjHaACu22pEp5CUoSgFCwHYTfUE3aPKPgwccAJqneZoHX1J6iRvkkbxTuYdNhGSUHqfoWzDy
+//! yprvABrGsX5C9jant7STRdfpNKYoQEabbJueE7gvWgU3auPusLdJT8y1JJs5JkRFU8hVnxWVh6CimW9CT5u5izWjEASMUJAL8YCBYAXwEGcYVem
+//! zprvAWgYBBk7JR8GjQdaFzTSaQeJaCj3Xvu99ED9J5MvxumnvSSXho8ZvNXDKxNqU3MRCbdJSZoHEAVkLNWeSgvk2Q7xLdrkiT1fotbacrsgQox
+//! ```
+//!
 //! ## Features
 //!
 //! * **Generate a list of valid last words**
 //!
 //!     Insert a mnemonic missing the last word and a list of possible valid last words (that
 //!  fulfill the checksum) is showed.
+//!
 //! * **Generate extended keys**
 //!
 //!     Insert entropy or mnemonic to show extended root keys.
@@ -60,6 +81,40 @@
 //!     Insert mnemonic and target language flag to result in words in the same 'position' on the
 //!  new word-list.
 //!
+//! ## Help
+//!
+//! ```bash
+//! mnemonic39 1.0.2
+//! Enter optional language and hexadecimal entropy with optional passphrase or
+//! enter a list of mnemonic words with optional passphrase to see information
+//! about it. It can show a list of valid last words to fulfil a list of mnemonic
+//! words missing the last one. Generate mnemonic using a list of valid words.
+//!
+//! USAGE:
+//!     mnemonic39 [FLAGS] [OPTIONS] <DATA>...
+//!
+//! FLAGS:
+//!     -c, --chinese        Mnemonic with chinese simplified words
+//!     -e, --english        Mnemonic with english words
+//!     -f, --french         Mnemonic with french words
+//!     -g, --generate       Generate valid mnemonics permuting words
+//!     -h, --help           Prints help information
+//!     -i, --italian        Mnemonic with italian words
+//!     -j, --japanese       Mnemonic with japanese words
+//!     -k, --korean         Mnemonic with korean words
+//!     -o, --portuguese     Mnemonic with portuguese words
+//!     -s, --spanish        Mnemonic with spanish words
+//!     -t, --traditional    Mnemonic with chinese traditional words
+//!     -V, --version        Prints version information
+//!     -z, --czech          Mnemonic with czech words
+//!
+//! OPTIONS:
+//!     -p <passphrase>        optional passphrase to be used with the mnemonic
+//!
+//! ARGS:
+//!     <DATA>...    hexadecimal entropy, seed or a list of mnemonic words
+//! ```
+//!
 //! ## What this tool *don't* do
 //! * **Generate entropy**
 //!
@@ -97,8 +152,7 @@ const ABOUT: &str =
 "Enter optional language and hexadecimal entropy with optional passphrase or
 enter a list of mnemonic words with optional passphrase to see information
 about it. It can show a list of valid last words to fulfil a list of mnemonic
-words informed missing the last one. Generate mnemonic using a list of valid
-words.";
+words missing the last one. Generate mnemonic using a list of valid words.";
 
 /// Number of bits represented in a single mnemonic word.
 const BITS_WORD: usize = 11;
